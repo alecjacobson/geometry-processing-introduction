f7b4550d618498c9975a5855e95b7be72a03b11b
Alec Jacobson <alecjacobson@gmail.com>
2017-01-11 00:05:39 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "edges.h"
+#include "euler_characteristic.h"
+#include <igl/read_triangle_mesh.h>
+#include <igl/viewer/Viewer.h>
+
+int main(int argc, char *argv[])
+{
+  Eigen::MatrixXd V;
+  Eigen::MatrixXi F;
+  // Load in a mesh
+  igl::read_triangle_mesh(argc>1 ? argv[1] : "data/bunny.off", V, F);
+
+  Eigen::MatrixXi E = edges(F);
+  int Chi = euler_characteristic(F);
+  std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
+  std::cout<<"Euler Characteristic: "<<Chi<<std::endl;
+
+  // Create a libigl Viewer object 
+  igl::viewer::Viewer viewer;
+  // Set the vertices and faces for the viewer
+  viewer.data.set_mesh(V, F);
+  // Launch a viewer instance
+  viewer.launch();
+  return 0;
+}
+
