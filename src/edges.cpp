0f44e0fa9d9dc314e23050adae238148f0ce187f
Tian Ze (Peter) Chen <hyswpc@gmail.com>
2017-01-16 01:45:31 -0500

    Homework 00, submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,26 @@
 #include "edges.h"
-
+#include <set>
+#include <algorithm>
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  typedef std::set<std::pair<int, int> > EdgeSet;
+  EdgeSet es;
+  for ( int i = 0 ; i < F.rows() ; ++i ) {
+    for ( int j = 0 ; j < 3 ; ++j ) {
+      if ( F(i, j) < F(i, (j+1)%3) ) {
+        es.insert(std::pair<int, int>(F(i, j), F(i, (j+1)%3)));
+      } else {
+        es.insert(std::pair<int, int>(F(i, (j+1)%3), F(i, j)));
+      }
+    }
+  }
+  E.resize(es.size(), 2);
+  int offset = 0;
+  for ( EdgeSet::iterator i = es.begin() ; i != es.end() ; ++i ) {
+    E(offset, 0) = i->first;
+    E(offset, 1) = i->second;
+    offset++;
+  }
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,17 @@
+#include "edges.h"
 #include "euler_characteristic.h"
-
+#include <set>
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+  typedef std::set<int> VertexSet;
+  VertexSet vs;
+  for ( int i = 0 ; i < F.rows() ; ++i ) {
+    for ( int j = 0 ; j < 3 ; ++j ) {
+      vs.insert(F(i, j));
+    }
+  }
+  Eigen::MatrixXi E = edges(F);
+  Chi = vs.size() - E.rows() + F.rows();
   return Chi;
 }
