cca35f079bb1823da1901c0d071995ded6a27d0e
AdamSturge <AdamSturge991@hotmail.com>
2017-01-17 13:12:13 -0500

    sign error in Euler Charateristic

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -5,6 +5,6 @@ int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   Eigen::MatrixXi E = edges(F);
-  Chi = F.maxCoeff() + 1 + E.rows() + F.rows(); //Assuming all vertices are used to form triangles, then maxCoeff + 1 is number of vertices (zero indexing)
+  Chi = (F.maxCoeff() + 1) - E.rows() + F.rows(); //Assuming all vertices are used to form triangles, then maxCoeff + 1 is number of vertices (zero indexing)
   return Chi;
 }
