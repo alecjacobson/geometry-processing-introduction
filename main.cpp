b6810559b29dab40cdb4b1cf5ab0f87285e92b94
ast <ast@Planck.local>
2017-01-17 22:58:19 -0500

    Assigment 1. Adding to routines for calculating unique "undirected" edges and the euler_characteristic of a polyhedral surface mesh. Debugging code to visualize edges added, see main.cpp comments.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ int main(int argc, char *argv[])
   // Load in a mesh
   igl::read_triangle_mesh(argc>1 ? argv[1] : "../shared/data/bunny.off", V, F);
 
+  //std::cout<<"There are "<<F.rows()<<" faces and x3 edges = "<<F.rows()*3<<std::endl;
+  std::cout<<V.rows()<<" read vertices"<<std::endl;
+  
   Eigen::MatrixXi E = edges(F);
   int Chi = euler_characteristic(F);
   std::cout<<"Edge list E is "<<E.rows()<<"x"<<E.cols()<<std::endl;
@@ -19,7 +22,14 @@ int main(int argc, char *argv[])
   igl::viewer::Viewer viewer;
   // Set the vertices and faces for the viewer
   viewer.data.set_mesh(V, F);
-  // Launch a viewer instance
+  
+  // Debugging - Display the "unique" edges only - toggle comment
+  // with the set_mesh above
+  // Eigen::MatrixXd C( E.rows(), 3 );
+  // C = Eigen::MatrixXd::Zero( E.rows(), 3 ); // set colors to black
+  // viewer.data.set_edges(V, E, C);
+
+// Launch a viewer instance
   viewer.launch();
   return 0;
 }
