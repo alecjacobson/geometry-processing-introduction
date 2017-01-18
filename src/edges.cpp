d691a33e57b8845f2ca8ebdf5e6c3ca717a065b3
Rinat Abdrashitov <rindopuz23@gmail.com>
2017-01-17 19:59:52 -0500

    H0 submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,51 @@
 #include "edges.h"
+#include <unordered_set>
+
+struct Edge
+{
+    int v1,v2;
+    
+    Edge(int v1, int v2) : v1(v1), v2(v2) {}
+    
+    int getId() const {return v1+v2;}
+    
+    bool operator ==(const Edge &other) const {
+        return ((other.v1 == this->v1) && (other.v2 == this->v2)) ||
+               ((other.v2 == this->v1) && (other.v1 == this->v2));
+    }
+};
+
+namespace std {
+    template<> struct hash<Edge> {
+        size_t operator()(const Edge & obj) const {
+            return hash<int>()(obj.getId());
+        }
+    };
+}
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+
+  std::unordered_set<Edge> edgeSet;
+  for (int i = 0; i < F.rows(); ++i) 
+  {
+  	edgeSet.insert(Edge(F(i,0), F(i,1)));
+  	edgeSet.insert(Edge(F(i,0), F(i,2)));
+  	edgeSet.insert(Edge(F(i,1), F(i,2)));
+  }
+
+  int numEdges = edgeSet.size();
+  //printf("Num of Edges %i\n", numEdges);
+  E.resize(numEdges,2);
+
+  int row = 0;
+  for (const auto& elem : edgeSet) 
+  {
+  	E(row, 0) = elem.v1;
+  	E(row++, 1) = elem.v2;
+  }
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,27 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <climits>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
+  // Chi = |V| - |E| + |F|.
+  
+  Eigen::MatrixXi E = edges(F);
+  int E_num = E.rows();
+  int F_num = F.rows();
+  int V_num = INT_MIN;
+
+  //find maximum vertex index which is equal to number of verticies in total minus 1
+  for (int i = 0; i < E_num; ++i) 
+  {
+  	V_num = (V_num < E(i,0)) ? E(i,0) : V_num;
+  	V_num = (V_num < E(i,1)) ? E(i,1) : V_num;
+  }
+
+  V_num++; //since indexing starts from 0
+  Chi = V_num - E_num + F_num;
+
   return Chi;
 }
