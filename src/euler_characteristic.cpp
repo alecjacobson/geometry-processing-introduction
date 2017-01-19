59f160b59fc7e9a4a0f43988012783ae9b0104f0
zhangpan721 <zhangpan721@hotmail.com>
2017-01-19 00:07:12 -0500

    Update euler_characteristic

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,21 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <set>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
+
+  //use set again for uniqueness
+  std::set<int> vertex;
+  Eigen::MatrixXi E = edges(F);
+
+  for (int i = 0; i < E.rows(); ++i){  
+        vertex.insert( E(i,0) );
+        vertex.insert( E(i,1) );
+  }
+
+  Chi = vertex.size() - E.rows() + F.rows();
   return Chi;
 }
