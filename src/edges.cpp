bacab5e1e5cc6e904e5d4db2417943cf97513013
sarahanne <sak@dgp.toronto.edu>
2018-01-15 18:14:10 -0500

    assignment works, testing on other meshes now

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,35 @@
 #include "edges.h"
+#include <igl/unique.h>
+#include <igl/sort.h>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
+  Eigen::MatrixXi E(F.rows()*3, 2);
   // ADD YOUR CODE HERE
+  
+  int j = 0;
+  for (int i = 0; i < F.rows(); i++)
+  {
+	  E(j, 0) = F(i, 0);
+	  E(j, 1) = F(i, 1);
+	  E(j+1, 0) = F(i, 1);
+	  E(j+1, 1) = F(i, 2);
+	  E(j+2, 0) = F(i, 2);
+	  E(j+2, 1) = F(i, 0);
+	  j += 3;
+  }
+
+  Eigen::MatrixXi Y;
+  Eigen::MatrixXi IX;
+
+  igl::sort(E, 2, true, Y, IX);
+
+  Eigen::MatrixXi C;
+  Eigen::VectorXi IA;
+  Eigen::VectorXi IC;
+  igl::unique_rows(Y, C, IA, IC);
+
+  E = C;
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,20 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <iostream>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+
+  int nE = E.rows();
+  int nF = F.rows();
+  int nV = E.maxCoeff()+1;
+
+  //std::cout << nV << " - " << nE << " + " << nF<< std::endl;
+
+  Chi = nV - nE + nF;
+
   return Chi;
 }
