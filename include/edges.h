b24fbfac15770eb4c347c480fe709519d35618d1
Michelle <m.arkhangorodsky@gmail.com>
2018-01-16 00:16:41 -0500

    edges.cpp implemented, needs refactoring + speedup

--- include/edges.h
+++ include/edges.h
@@ -1,6 +1,10 @@
 #ifndef EDGES_H
 #define EDGES_H
 #include <Eigen/Core>
+#include <iostream>
+#include <set>
+#include <tuple>
+
 // From a list of triangle indices into some vertex set V, determine the
 // set of unique undirected edges.
 //
@@ -8,4 +12,10 @@
 //   F  #F by 3 list of triangles indices into some vertex set V
 // Returns E  #E by 2 list of undirected edge indices into V
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F);
+
+// Adds the undirected edge (vertexA, vertexB) to E if it is not in seenEdges, and returns an updated E_size
+int addToEdges(Eigen::MatrixXi &E, int E_size, std::set<std::tuple<int, int>> &seenEdges, int vertexA, int vertexB);
+
+// Returns true if the undirected edge (vertexA, vertexB) is in seenEdges, otherwise false
+bool alreadySeen(std::set<std::tuple<int, int>> seenEdges, int vertexA, int vertexB);
 #endif
--- src/edges.cpp
+++ src/edges.cpp
@@ -3,6 +3,30 @@
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  int E_size = 0;
+  std::set<std::tuple<int, int>> seenEdges;
+  std::cout << "Size of F is " << F.rows() << std::endl;
+  for(int f_idx=0; f_idx < F.rows(); f_idx++){
+    E_size = addToEdges(E, E_size, seenEdges, F(f_idx,0), F(f_idx,1));
+    E_size = addToEdges(E, E_size, seenEdges, F(f_idx,0), F(f_idx,2));
+    E_size = addToEdges(E, E_size, seenEdges, F(f_idx,1), F(f_idx,2));
+  }
+  std::cout << E_size << std::endl; //10449
   return E;
+}
+
+int addToEdges(Eigen::MatrixXi &E, int E_size, std::set<std::tuple<int, int>> &seenEdges, int vertexA, int vertexB){
+  if(!alreadySeen(seenEdges, vertexA, vertexB)){
+    E_size += 1;
+    E.resize(E_size, 2);
+    E(E_size-1, 0) = vertexA;
+    E(E_size-1,1) = vertexB;
+    seenEdges.insert(std::make_tuple(vertexA, vertexB));
+  }
+  return E_size;
+}
+bool alreadySeen(std::set<std::tuple<int, int>> seenEdges, int vertexA, int vertexB){
+  std::tuple<int,int> edge = std::make_tuple(vertexA, vertexB);
+  std::tuple<int,int> reversedEdge = std::make_tuple(vertexB, vertexA);
+  return (seenEdges.find(edge) != seenEdges.end()) || (seenEdges.find(reversedEdge) != seenEdges.end());
 }
