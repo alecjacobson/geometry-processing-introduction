eebdba519a2a25eb26eab90795e04a5ec45cea8a
KENAN DENG <dknyxh@gmail.com>
2018-01-16 21:11:56 -0500

    add some comments

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -4,10 +4,10 @@ int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
-  int num_F = F.rows();
-  Eigen::MatrixXi e_matrix = edges(F);
-  int num_E = e_matrix.rows();
-  int num_V = F.maxCoeff() + 1;
-  Chi = num_V - num_E + num_F;
+  int num_F = F.rows(); //number of faces
+  Eigen::MatrixXi e_matrix = edges(F); 
+  int num_E = e_matrix.rows(); //number of edges
+  int num_V = F.maxCoeff() + 1; //number of vertices, assume no unreferenced vertices
+  Chi = num_V - num_E + num_F; 
   return Chi;
 }
