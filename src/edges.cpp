ec91c0af11f71f4e8e3b091ea4ee103e63adda6f
sharm146 <shash.sharma@mail.utoronto.ca>
2018-01-17 10:42:11 -0500

    completed assigment.

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,65 @@
 #include "edges.h"
+#include <vector> // Not sure if this is allowed; if it isn't, I'll know for future assignments!
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  
+  // First, pretend all the triangles are unconnected, so that every edge is unique.
+  // So we can go through all the triangles' vertices, and store each edge.
+  // Since we only care about undirected edges, just sort each pair before putting it
+  // in the list. This will make it easier to remove duplicates later on.
+  // If the vertices of a face are indexed as 0, 1 ,2, then we know that the
+  // edges would be (0,1), (1,2), (2,0) for that face.
+
+  // Create a temporary container for the non-unique edges. There will be 3 for each face.
+  std::vector<std::vector<int>> edges_with_duplicates (F.rows()*3);
+
+  // Iterate through the matrix of faces
+  int ee = 0; // keeps track of the rows of E, which are different from rows of F
+  for (int ii = 0; ii < F.rows(); ii++)
+  {
+  	// First edge
+  	edges_with_duplicates[ee].push_back(F(ii, 0));
+  	edges_with_duplicates[ee].push_back(F(ii, 1));
+  	std::sort(edges_with_duplicates[ee].begin(), edges_with_duplicates[ee].end());
+  	ee++;
+
+  	// Second edge
+  	edges_with_duplicates[ee].push_back(F(ii, 1));
+  	edges_with_duplicates[ee].push_back(F(ii, 2));
+  	std::sort(edges_with_duplicates[ee].begin(), edges_with_duplicates[ee].end());
+  	ee++;
+
+  	// Third edge
+  	edges_with_duplicates[ee].push_back(F(ii, 2));
+  	edges_with_duplicates[ee].push_back(F(ii, 0));
+  	std::sort(edges_with_duplicates[ee].begin(), edges_with_duplicates[ee].end());
+  	ee++;
+  }
+
+  // Now we have to remove duplicates. Using C++ vector methods for this.
+  // the vectors have to be sorted first.
+  // (source: https://stackoverflow.com/questions/2512978/how-to-delete-duplicate-vectors-within-a-multidimensional-vector)
+  std::sort(edges_with_duplicates.begin(), edges_with_duplicates.end());
+  edges_with_duplicates.erase(std::unique(edges_with_duplicates.begin(), edges_with_duplicates.end()), edges_with_duplicates.end());
+
+  // Now copy over the edges to the output matrix.
+  // There is probably a more efficient way to do this by giving the Eigen matrix
+  // a reference to contiguously stored data in the vector, but I think the
+  // vector<vector> would have stored things resembling row major, while based on
+  // Eigen's documentation, the output matrix would want it to be in column major.
+
+  E.resize(edges_with_duplicates.size(), 2);
+  for (int ii = 0; ii < edges_with_duplicates.size(); ii++)
+  {
+  	for (int jj = 0; jj < 2; jj++)
+  	{
+  	  E(ii, jj) = edges_with_duplicates[ii][jj];
+  	}
+  }
+
   return E;
 }
+
+
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,41 @@
 #include "euler_characteristic.h"
+#include <vector>
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+
+  // Call the edges function to get the list of unique edges
+  Eigen::MatrixXi E = edges(F);
+
+  // Use the formula for polyhedral surfaces
+  int N_faces = F.rows();
+  int N_edges = E.rows();
+
+  // To find the total number of unique vertices, go through all the vertices
+  // in the faces, put them in one big vector, then sort and remove duplicates.
+  std::vector<int> vertices;
+  for (int ii = 0; ii < N_faces; ii++)
+  	for (int jj = 0; jj < 3; jj++)
+  	{
+  		vertices.push_back(F(ii,jj));
+  	}
+
+  std::sort(vertices.begin(), vertices.end());
+  vertices.erase(std::unique(vertices.begin(), vertices.end()), vertices.end());
+
+  int N_vert = vertices.size();
+
+  // Apply the formula
+  Chi = N_vert - N_edges + N_faces;
+
+  // Test
+  // std::cout << Chi << std::endl;
+
+  // Validation: based on some research online, the euler characteristic for
+  // any closed surface should be 2. The bunny looks like it is a closed surface,
+  // and this function returns 2, so it seems to be working.
+
   return Chi;
 }
