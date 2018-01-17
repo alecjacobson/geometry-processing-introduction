049a0af1fe28646c86c1455f6501e1337e0235e6
HTDerekLiu <htliu1992@gmail.com>
2018-01-16 23:11:29 -0500

    add code

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,29 @@
 #include "edges.h"
 
-Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
-{
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+Eigen::MatrixXi edges(const Eigen::MatrixXi &F){
+	Eigen::MatrixXi E;
+	// ADD YOUR CODE HERE
+	// Step1: construct unique edge list
+	map<pair<int,int>, int> uniqueEdge; // the third int is the useless variable
+	for (int r = 0; r < F.rows(); r++){ // Iterator all faces
+		for (int idx = 0; idx < 3; idx++){ 
+			// Extract two vertices
+			int v1 = F(r, (0+idx)%3);
+			int v2 = F(r, (1+idx)%3);
+			if (v1 < v2) swap(v1, v2); // make sure v1 > v2
+			pair<int,int> e(v1, v2);
+			if (uniqueEdge.find(e) == uniqueEdge.end()) uniqueEdge[e] = 0;
+		}
+	}
+
+	// Step2: add unique edge to Eigen::MatrixXi
+	E.resize(uniqueEdge.size(), 2);
+	int rowIdx = 0;
+	for (auto it = uniqueEdge.begin(); it != uniqueEdge.end(); it++){
+		E(rowIdx, 0) = it->first.first;
+		E(rowIdx, 1) = it->first.second;
+		rowIdx++;
+	}
+
+	return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -2,7 +2,17 @@
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi;
-  // ADD YOUR CODE HERE
-  return Chi;
+	// ADD YOUR CODE HERE
+  	Eigen::MatrixXi E = edges(F);
+	int numEdge = E.rows();
+	int numFace = F.rows();
+
+	// Compute number of vertices from faces
+	std::unordered_map<int, int> uniqueVert;
+	for (int i = 0; i < E.rows(); i++){
+		if (uniqueVert.find(E(i,0)) == uniqueVert.end()) uniqueVert[E(i,0)] = 1;
+		if (uniqueVert.find(E(i,1)) == uniqueVert.end()) uniqueVert[E(i,1)] = 1;
+	}  
+	int numVert = uniqueVert.size();
+	return numVert - numEdge + numFace;
 }
