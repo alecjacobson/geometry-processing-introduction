4d1987ea6c35aee52aa6ff9de29967b3305d78d7
Ziheng Liang <ziheng941008@hotmail.com>
2018-01-17 14:49:53 -0500

    use edges function in euler_characteristic

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,10 +1,11 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
-  int E = F.rows() * 3.0 / 2.0;
+  int E = edges(F).rows();
   int V = F.maxCoeff() + 1;
   int FNum = F.rows();
   Chi = V - E + FNum;
