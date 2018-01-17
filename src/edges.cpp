516ddb6dbeb14fa68c0721eea6c1d8524fbd823d
zero-impact <lawsonfulton@gmail.com>
2018-01-17 13:09:12 -0500

    Finished first assignment

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,30 @@
 #include "edges.h"
 
+#include <set>
+#include <vector>
+#include <algorithm>
+
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  
+  std::set<std::pair<int, int>> edge_set;
+  for(int i = 0; i < F.rows(); i++) {
+    for(int j = 0; j < F.cols(); j++) {
+        int v1 = F(i, j);
+        int v2 = F(i, (j + 1) % F.cols());
+
+        if(v1 > v2) std::swap(v1, v2);
+
+        edge_set.insert(std::make_pair(v1, v2));
+    }
+  }
+
+  E = Eigen::MatrixXi(edge_set.size(), 2);
+  int i = 0;
+  for(auto edge : edge_set) {
+    E.row(i++) = Eigen::Vector2i(edge.first, edge.second);
+  }
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,16 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+
+  Eigen::MatrixXi E = edges(F);
+  int n_V = F.maxCoeff() + 1;
+  int n_E = E.rows();
+  int n_F = F.rows();
+
+  Chi = n_V - n_E + n_F;
+
   return Chi;
 }
