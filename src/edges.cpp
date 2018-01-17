bd2ddc67d80c16daf686686f2a85fbc4a7db4562
andrew <andrew.pelegris@gmail.com>
2018-01-16 22:39:52 -0500

    Implemented the work for this excersise.

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,29 @@
+#include <vector>
+#include <set>
 #include "edges.h"
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+	Eigen::MatrixXi E;
+
+	// Make sure edges are unique by generating a set.
+	std::set<std::pair<int, int>> edgeSet;
+	int faceCount = F.rows();
+	for (int i = 0; i < faceCount; i++) {
+		edgeSet.insert(std::pair<int, int>(F(i, 0), F(i, 1)));
+		edgeSet.insert(std::pair<int, int>(F(i, 1), F(i, 2)));
+		edgeSet.insert(std::pair<int, int>(F(i, 2), F(i, 0)));
+	}
+
+	// Convert set into a vector so that we can use MatrixXi::Map(...).
+	std::vector<int> edgeData;
+	for (auto edge : edgeSet) {
+		edgeData.push_back(edge.first);
+		edgeData.push_back(edge.second);
+	}
+
+	int edgeCount = edgeSet.size();
+	E = Eigen::MatrixXi::Map(&edgeData.front(), edgeCount, 2);
+
+	return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,27 @@
+#include <set>
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi;
-  // ADD YOUR CODE HERE
-  return Chi;
+
+	int faceCount = F.rows();
+
+	Eigen::MatrixXi theEdges = edges(F);
+	int edgeCount = theEdges.rows();
+
+	// Have to calculate set of vertex indices so that we can get the correct
+	// vertex count.
+	// Looks like vertices were already indexed nicely, but oh well...
+	std::set<int> vertexSet;
+	for (int i = 0; i < faceCount; i++) {
+		vertexSet.insert(F(i, 0));
+		vertexSet.insert(F(i, 1));
+		vertexSet.insert(F(i, 2));
+	}
+	int vertexCount = vertexSet.size();
+
+	// Formula from the readme. Assumes polyhedral surfaces.
+	int Chi = vertexCount - edgeCount + faceCount;
+	return Chi;
 }
