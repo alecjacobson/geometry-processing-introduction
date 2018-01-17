12edc8f23d0d238f1d1d8f4ba21bcecdeff62c12
KENAN DENG <dknyxh@gmail.com>
2018-01-16 20:35:21 -0500

    finish

--- src/edges.cpp
+++ src/edges.cpp
@@ -14,19 +14,19 @@ Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
   		int start = F(i,j);
   		int end = F(i,(j+1) % 3);
   		bool duplicate = false;
-  		if (rmap.find(start) != rmap.end() ) {
+  		if (rmap.find(start) != rmap.end() ) { //check if already have this edge
   			std::vector<int> v = rmap[start];
   			if (std::find(v.begin(), v.end(), end) != v.end()){
   				duplicate = true;
   			}
 		  }
-		  if (!duplicate && rmap.find(end) != rmap.end()){
+		  if (!duplicate && rmap.find(end) != rmap.end()){ //the other way around
   			std::vector<int> v = rmap[end];
   			if (std::find(v.begin(), v.end(), start) != v.end()){
   				duplicate = true;
     		}
   		}
-    	if (!duplicate){
+    	if (!duplicate){ // add the edge
   			v_start.push_back(start);
   			v_end.push_back(end);
   			if (rmap.find(start) != rmap.end()){
@@ -39,7 +39,7 @@ Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
   	}
   }
   E.resize(v_start.size(),2);
-  for (int i = 0; i<v_start.size(); ++i){
+  for (int i = 0; i<v_start.size(); ++i){ //add to the matrix
     E(i,0) = v_start[i];
     E(i,0) = v_end[i];
   }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,14 @@
 #include "euler_characteristic.h"
-
+#include "edges.h"
+#include <igl/euler_characteristic.h>
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
   // ADD YOUR CODE HERE
+  int num_F = F.rows();
+  Eigen::MatrixXi e_matrix = edges(F);
+  int num_E = e_matrix.rows();
+  int num_V = F.maxCoeff() + 1;
+  Chi = num_V - num_E + num_F;
   return Chi;
 }
