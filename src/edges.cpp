7068a8d9dce1d87d34ef0d2686c8cad2de5a4a95
sajjadn <nezhadi.sajjad@gmail.com>
2018-01-17 19:37:18 -0500

    asmt 0

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,36 @@
 #include "edges.h"
+#include <iostream> 
+#include <algorithm>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  int n = F.maxCoeff() + 1;
+  Eigen::MatrixXi E (3*n, 2);
+  // Temporary matrix to house edges
+  Eigen::MatrixXi edge (n, n);
+
+// Order nodes from min to max for each Face
+// Save edges with that ordering
+  for (int i = 0; i < F.rows(); i = i + 1) {
+  	int a = F.row(i).minCoeff();
+  	int c = F.row(i).maxCoeff();
+  	int b = F(i, 0) + F(i, 1) + F(i, 2) - a - c;
+  	edge(a, b) = 1;
+  	edge(a, c) = 1;
+  	edge(b, c) = 1;
+}
+// Fill E Matrix with edges
+  int numE = 0;
+
+  for (int i = 0; i < n; i = i + 1) {
+  	for (int j = 0; j < n; j = j + 1) {
+  	if (edge(i, j) == 1){
+      numE += 1;
+  		E(i, 0) = i;
+   		E(i, 1) = j;
+  }
+}
+}
+  E.resize(numE,2);
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,13 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+    int Fcard = F.rows();
+    int Ecard = E.rows();
+    int Vcard = F.maxCoeff() + 1;
+    Chi = Vcard - Ecard + Fcard;
   return Chi;
 }
