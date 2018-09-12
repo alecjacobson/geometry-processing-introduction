d416eb0a87ded0c4a5a5fdc52641bd2ffab96b21
Simeon Krastnikov <simeon.krastnikov@mail.utoronto.ca>
2018-09-12 16:48:33 -0400

    HW0 Submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,34 @@
 #include "edges.h"
+#include <vector>
+using namespace std;
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  
+  int num_faces = F.rows();
+  int num_vertices = F.maxCoeff() + 1;
+  
+  vector<int> adj[num_vertices];
+  vector<int> edge_list;
+  
+  for (int i = 0; i < num_faces; i++) {
+    for (int j = 0; j < 3; j++) {
+      int v1 = F(i, j);
+      int v2 = F(i, (j+1) % 3);
+      if (find(adj[v1].begin(), adj[v1].end(), v2) == adj[v1].end()) {
+        adj[v1].push_back(v2);
+        adj[v2].push_back(v1);
+        edge_list.push_back(v1);
+        edge_list.push_back(v2);
+      }
+    }
+  }
+  
+  E.resize(edge_list.size() / 2, 2);
+  for (int i = 0; i < edge_list.size(); i++) {
+    E(i / 2, i % 2) = edge_list[i];
+  }
+  
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,11 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi = 0;
-  // ADD YOUR CODE HERE
-  return Chi;
+  int f_count = F.rows();
+  int v_count = F.maxCoeff() + 1;
+  int e_count = edges(F).rows();
+  
+  return v_count - e_count + f_count;
 }
