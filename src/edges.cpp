43763109da5546c9450cea424aef3290d719c24b
Feilong Song <feilong.song@mail.utoronto.ca>
2018-09-11 21:22:25 -0400

    Finished edges and euler.

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,34 @@
 #include "edges.h"
+#include <iostream>
+#include <set>
+#include <array>
 
-Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
-{
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+using namespace std;
+
+Eigen::MatrixXi edges(const Eigen::MatrixXi &F) {
+    Eigen::MatrixXi E;
+    // ADD YOUR CODE HERE
+    set<array<int, 2>> edges;
+
+    for (int i = 0; i < F.rows(); i++) {
+        array<int, 3> tmp = {F(i, 0), F(i, 1), F(i, 2)};
+        sort(tmp.begin(), tmp.end());
+
+        array<int, 2> edge0 = {tmp[0], tmp[1]};
+        array<int, 2> edge1 = {tmp[0], tmp[2]};
+        array<int, 2> edge2 = {tmp[1], tmp[2]};
+
+        edges.insert(edge0);
+        edges.insert(edge1);
+        edges.insert(edge2);
+    }
+
+    E.resize(edges.size(), 2);
+    int i = 0;
+    for (auto it = edges.begin(); it != edges.end(); it++, i++) {
+        E(i, 0) = (*it)[0];
+        E(i, 1) = (*it)[1];
+    }
+
+    return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,21 @@
+#include <edges.h>
 #include "euler_characteristic.h"
+#include <set>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+
+  // Calculate V
+  std::set<int> vertices;
+  for (int i = 0; i < F.rows(); i++) {
+    vertices.insert(F(i, 0));
+    vertices.insert(F(i, 1));
+    vertices.insert(F(i, 2));
+  }
+
+  Chi = (int) (vertices.size() - E.rows() + F.rows());
   return Chi;
 }
