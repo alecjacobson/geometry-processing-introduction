b7eec10fdb5849f8ef565c29708f4f07f3c54e99
Zhiyuan <jerryz.cheng@mail.utoronto.ca>
2018-09-15 15:30:53 -0400

    submit HW0

--- src/edges.cpp
+++ src/edges.cpp
@@ -2,7 +2,27 @@
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+	Eigen::MatrixXi E;
+	int counter = 0;
+	std::vector<std::vector<int>> all_edges;
+	// ADD YOUR CODE HERE	
+	E.conservativeResize(E.rows(), E.cols() + 2);
+	for (int i = 0; i < F.rows(); i++) {
+		for (int j = 0; j < 3; j++) {
+			std::vector<int> edge;
+			edge.push_back(F(i, j));
+			edge.push_back(F(i, (j + 1) % 3));
+			if (std::find(all_edges.begin(), all_edges.end(), edge) == all_edges.end()) {
+				std::reverse(edge.begin(), edge.end());
+				if (std::find(all_edges.begin(), all_edges.end(), edge) == all_edges.end()) {
+					E.conservativeResize(E.rows() + 1, E.cols());
+					E(counter, 0) = F(i, j);
+					E(counter, 1) = F(i, (j + 1) % 3);
+					all_edges.push_back(edge);
+				}
+			}
+		}
+		counter++;
+	}
+	return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,20 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi = 0;
-  // ADD YOUR CODE HERE
-  return Chi;
+	int Chi = 0;
+	// ADD YOUR CODE HERE
+	// Chi = |V| - |E| + |F| =  2
+	Eigen::MatrixXi E = edges(F);
+	std::vector<int> V;
+	for (int i = 0; i < E.rows(); i++) {
+		for (int j = 0; j < 2; j++) {
+			if (std::find(V.begin(), V.end(), E(i, j)) == V.end()) {
+				V.push_back(E(i, j));
+			}
+		}
+	}
+	Chi = V.size() - E.rows() + F.rows();
+	return Chi;
 }
