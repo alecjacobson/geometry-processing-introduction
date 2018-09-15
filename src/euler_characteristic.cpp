c4ab662b7556876d27b5326ca3a39805f2043850
Jerry Cheng <czydu@hotmail.com>
2018-09-15 16:29:33 -0400

    Update euler_characteristic.cpp

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -8,10 +8,10 @@ int euler_characteristic(const Eigen::MatrixXi &F)
 	// Chi = |V| - |E| + |F| =  2
 	Eigen::MatrixXi E = edges(F);
 	std::vector<int> V;
-	for (int i = 0; i < E.rows(); i++) {
+	for (int i = 0; i < F.rows(); i++) {
 		for (int j = 0; j < 2; j++) {
-			if (std::find(V.begin(), V.end(), E(i, j)) == V.end()) {
-				V.push_back(E(i, j));
+			if (std::find(V.begin(), V.end(), F(i, j)) == V.end()) {
+				V.push_back(F(i, j));
 			}
 		}
 	}
