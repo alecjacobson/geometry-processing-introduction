2735ab5ed1637ec953482e11f2762b9ce3a0af53
Yue Jiang <yuejiang@Yues-MBP.fios-router.home>
2018-09-15 16:46:16 -0400

    finished Assignment 0

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
 #include "euler_characteristic.h"
+#include "edges.h"
+
+using namespace std;
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi = 0;
-  // ADD YOUR CODE HERE
-  return Chi;
+	// Chi = |V| - |E| + |F|
+  	int Chi = (F.maxCoeff() + 1) - edges(F).rows() + F.rows();
+
+  	return Chi;
 }
