d20000b6e46a2b74520a1727d0da1d66ae4f88b9
Minhua Zhu <minhua.zhu@mail.utornoto.ca>
2018-09-16 18:58:03 -0400

    Minhua Zhu HW00

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,29 @@
 #include "edges.h"
+#include <igl/sort.h>
+#include <igl/unique_rows.h>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  Eigen::MatrixXi Y, I, Eall;
+  
+  igl::sort(F,2,true,Y,I);
+  for (int i = 0; i < Y.rows(); i++) {
+    //igl::sort(F.row(i),1,true,Y,I);
+    //std::cout << Y.row(i) << "\n";
+    int rowIndex = Eall.rows();
+    Eall.conservativeResize(rowIndex+3, 2);
+    Eall(rowIndex,0) = Y(i,0);
+    Eall(rowIndex,1) = Y(i,1);
+    Eall(rowIndex+1,0) = Y(i,0);
+    Eall(rowIndex+1,1) = Y(i,2);
+    Eall(rowIndex+2,0) = Y(i,1);
+    Eall(rowIndex+2,1) = Y(i,2);
+    
+  } // we get all edges now.
+
+  Eigen::MatrixXi IE, IEall;
+  igl::unique_rows(Eall,E,IEall,IE);
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,15 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <igl/unique.h>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+
+  Eigen::MatrixXi V, E;
+  E = edges(F);
+  igl::unique(F,V);
+  Chi = V.cols() - E.cols() + F.cols();
   return Chi;
 }
