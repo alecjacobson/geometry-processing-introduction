56bf9703fecd9492b83faf134383bcf27edb4546
Ruijian An <rayruijian@gmail.com>
2018-09-16 09:28:53 -0400

    ruijian an hw00

--- src/edges.cpp
+++ src/edges.cpp
@@ -4,5 +4,39 @@ Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+  int start;
+  int end;
+  int index = 0;
+
+  for (int i = 0; i < F.rows(); i++){
+    for (int j = 0; j < 3; j++){
+      if (j == 0){
+        start = F(i, 0);
+        end = F(i, 1);
+      } else if (j == 1){
+        start = F(i, 0);
+        end = F(i, 2);
+      } else {
+        start = F(i, 1);
+        end = F(i, 2);
+      }
+
+      bool flag = true;
+      for (int k = 0; k < E.rows(); k++){
+        if (((start == E(k, 0)) && (end == E(k, 1))) || ((start == E(k, 1)) && (end == E(k, 0)))){
+          flag = false;
+        }
+      }
+
+      if (flag){
+        E.conservativeResize(index + 1, 2);
+        E(index, 0) = start;
+        E(index, 1) = end;
+        index++;
+      }
+
+
+    }
+  }
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,15 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+  int V_size = F.maxCoeff() + 1;
+  int E_size = E.rows();
+  int F_size = F.rows();
+  Chi = V_size - E_size + F_size;
+
   return Chi;
 }
