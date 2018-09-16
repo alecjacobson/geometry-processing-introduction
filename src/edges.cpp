8caf612f15c6ad3c44b8b99014537a7d72f79871
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2018-09-16 16:30:15 -0400

    update a faster version

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,40 +1,43 @@
 #include "edges.h"
+#include <vector>
+
+bool comp(Eigen::RowVector2i a, Eigen::RowVector2i b) {
+  return a(0) > b(0) || (a(0) == b(0) && a(1) > b(1));  
+}
+
+bool equal(Eigen::RowVector2i a, Eigen::RowVector2i b) {
+  return a(0) == b(0) && a(1) == b(1);
+}
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
 
-  int k = 0;
+  // There are 3 edges per face (including duplicates)
+  Eigen::MatrixXi preE_a(F.rows() * 3, 2), preE_b(F.rows() * 3, 2);
 
-  for (int i=0; i < F.rows(); i ++) {
-    for (int j=0; j < 3; j ++) {
+  // Assign values to them
+  preE_a << F.col(0), F.col(1),
+          F.col(1), F.col(2),
+          F.col(2), F.col(0);
 
-      // Construct current edge
-      Eigen::RowVector2i edge;
-      if (j == 2)
-        edge << F(i, j), F(i, 0);
-      else
-        edge << F(i, j), F(i, j + 1);
-      
-      // Check if current edge already in E
-      // Idea: two half-edge's have opposite 
-      // orientation on a consistently oriented mesh
-      Eigen::RowVector2i oppo_edge;
-      oppo_edge << edge(1), edge(0);
+  // start with higher indices
+  preE_b << preE_a.rowwise().maxCoeff(), preE_a.rowwise().minCoeff();
 
-      if (E.rows() != 0) {
-        if ( ((E.rowwise() - oppo_edge
-              ).cwiseAbs().rowwise().sum().array() == 0).any() ) {
-          continue;
-        }
-      }
+  // using sort and unique to remove duplicates
+  // Idea inspired by C++ reference http://www.cplusplus.com/reference/algorithm/unique/
+  std::vector<Eigen::RowVector2i> my_vector;
+  for (int i = 0; i < preE_b.rows(); i++)
+      my_vector.push_back(preE_b.row(i));
 
-      // Able to add a new edge
-      E.conservativeResize(k + 1, 2);
-      E(k, 0) = edge(0);
-      E(k, 1) = edge(1);
-      k ++;
-    }
+  std::sort(my_vector.begin(), my_vector.end(), &comp);
+  auto last = std::unique(my_vector.begin(), my_vector.end(), &equal);
+  my_vector.erase(last, my_vector.end()); 
+
+  // Since duplicates have been removed, construct E using my_vector
+  E.conservativeResize(my_vector.size(), 2);
+  for (int j = 0; j < my_vector.size(); j ++) {
+    E.row(j) = my_vector[j];
   }
   return E;
 }
