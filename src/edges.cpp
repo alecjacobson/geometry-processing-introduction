ab1810a76bab118dc47f710c9e1c439699527b65
pomonam <pomonam15@gmail.com>
2018-09-16 11:24:51 -0400

    HW0 Completetion

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,37 @@
 #include "edges.h"
+#include <utility>
+#include <set>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+
+  // Use std::set to avoid duplicates.
+  // Note: E should contain every undirected edge exactly once.
+  std::set<std::pair<int, int>> edge_set;
+  for (size_t i = 0, nRows = F.rows(); i < nRows; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      std::pair<int, int> current_pair;
+      // Place larger index at the end to identify undirected edges.
+        if (F(i, j) >= F(i, (j + 1) % 3)) {
+        current_pair = std::make_pair(F(i, j), F(i, (j + 1) % 3));
+      } else {
+        current_pair = std::make_pair(F(i, (j + 1) % 3), F(i, j));
+      }
+      edge_set.insert(current_pair);
+    }
+  }
+
+  // Push all elements to E.
+  E.resize(edge_set.size(), 2);
+  std::set<std::pair<int, int>>::iterator it;
+  int idx = 0;
+  for (it = edge_set.begin(); it != edge_set.end(); ++it)
+  {
+    E(idx, 0) = (*it).first;
+    E(idx, 1) = (*it).second;
+    idx++;
+  }
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,30 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <set>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
-  // ADD YOUR CODE HERE
+
+  // Obtain |V|.
+  std::set<int> V;
+  for (size_t i = 0, nRows = F.rows(); i < nRows; ++i)
+  {
+    V.insert(F(i, 0));
+    V.insert(F(i, 1));
+    V.insert(F(i, 2));
+  }
+  int v_size = V.size();
+
+  // Obtain |E|.
+  Eigen::MatrixXi E = edges(F);
+  int e_size = E.rows();
+
+  // Obtain |F|.
+  int f_size = F.rows();
+
+  // Chi = |V| - |E| + |F|.
+  Chi = v_size - e_size + f_size;
+
   return Chi;
 }
