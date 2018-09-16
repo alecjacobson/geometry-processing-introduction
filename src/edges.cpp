fb10d904731ec9f818c2e3914adc5cb08ab5715b
liupeiLu <liupei.lu@mail.utoronto.ca>
2018-09-16 00:12:45 -0400

    HW00 Liupei Lu Commit

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,34 @@
 #include "edges.h"
+#include "igl/unique_rows.h"
+
+void insertRawEdges(int start, int end, Eigen::MatrixXi &E, int index)
+{
+  int temp = 0;
+
+  if (start >= end)
+  {
+    temp = start;
+    start = end;
+    end = temp;
+  }
+
+  E.row(index) << start, end;
+}
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi TempE, IC, IA;
+  TempE.resize((int)F.rows() * 3, 2);
+  for (int i = 0; i < (int)F.rows(); i++)
+  {
+    insertRawEdges(F(i, 0), F(i, 1), TempE, i * 3);
+    insertRawEdges(F(i, 1), F(i, 2), TempE, i * 3 + 1);
+    insertRawEdges(F(i, 2), F(i, 0), TempE, i * 3 + 2);
+  }
+
+  igl::unique_rows(TempE, E, IA, IC);
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,15 @@
 #include "euler_characteristic.h"
+#include "igl/unique.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+  Eigen::MatrixXi V;
+  igl::unique(F, V);
+
+  Chi = V.rows() - E.rows() + F.rows();
   return Chi;
 }
