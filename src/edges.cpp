af03b33db7a0b3c466c34807b7a2a6d48832eb97
Adrian <adrian.she@alumni.ubc.ca>
2018-09-17 16:35:23 -0400

    Assignment Submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -2,7 +2,42 @@
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
+  Eigen::MatrixXi E_temp(F.rows() * 3, 2);
+  int max_vert_idx = 0;
   // ADD YOUR CODE HERE
+  for (int i = 0; i < F.rows(); i++) {
+      // add the 3 half-edges specified from a single face in F
+      E_temp(3 * i, 0) = F(i, 0);
+      E_temp(3 * i, 1) = F(i, 1);
+      E_temp(3 * i + 1, 0) = F(i, 1);
+      E_temp(3 * i + 1, 1) = F(i, 2);
+      E_temp(3 * i + 2, 0) = F(i, 2);
+      E_temp(3 * i + 2, 1) = F(i, 0);
+      int max_of_three = std::max(std::max(F(i,0), F(i,1)), F(i,2));
+      if (max_of_three > max_vert_idx) {
+	max_vert_idx = max_of_three;
+      }
+  } 
+
+  // Count number of unique edges and produce that list.
+  Eigen::MatrixXi Unique_Edge_Mat = Eigen::MatrixXi::Zero(max_vert_idx + 1, max_vert_idx + 1);
+  for (int i = 0; i < E_temp.rows(); i++) {
+	Unique_Edge_Mat(E_temp(i,0), E_temp(i,1)) = 1;
+  } 
+  int N_edges = Unique_Edge_Mat.sum() / 2; // Each half-edge has been counted twice
+
+
+  Eigen::MatrixXi E(N_edges, 2);
+  int curRow = 0;
+  for (int i = 0; i <= max_vert_idx; i++) {
+	for (int j = 0; j <= i; j++) {
+            if (Unique_Edge_Mat(i,j) == 1) {
+		E(curRow, 0) = i;
+		E(curRow, 1) = j;
+		curRow++;
+	  }
+	}
+  }
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,41 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
+
+// Assuming no vertices are unreferenced by F
+// for |V| - |E| + |F| formula to be valid.
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  Eigen::MatrixXi E = edges(F);
+  int N_edges = E.rows();
+  int N_faces = F.rows(); 
+
+  // List all vertices in the mesh
+  Eigen::VectorXi Vertices(F.rows() * 3);
+  for (int i = 0; i < F.rows(); i++) {
+ 	Vertices(3 * i) = F(i,0);
+	Vertices(3 * i + 1) = F(i,1);
+	Vertices(3 * i + 2) = F(i,2);
+  }
+
+  // Count the number of unique vertices
+  int max_vert_idx = 0;
+  for (int i = 0; i < Vertices.size(); i++) {
+	if (Vertices(i) > max_vert_idx) {
+		max_vert_idx = Vertices(i);	
+	}
+  }
+
+  Eigen::VectorXi Unique_Verts = Eigen::VectorXi::Zero(max_vert_idx + 1);
+  for (int i = 0; i < Vertices.size(); i++) {
+	Unique_Verts(Vertices(i)) = 1;
+  }
+  int N_verts = Unique_Verts.sum();
+
+
+  // Calculate chi using chi = |V| - |E| + |F|
+  Chi = N_verts - N_edges + N_faces; 
   return Chi;
 }
