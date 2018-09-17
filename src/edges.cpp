f311667b512dd923f87aa919b39c341a15c0f6a3
juliagilenko <jugilenko@gmail.com>
2018-09-16 22:22:42 -0400

    Implemented edges() and euler_characteristic()

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,34 @@
 #include "edges.h"
+#include <tuple>
+#include <set>
+
+using namespace std;
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+    set<tuple<int, int>> edges;
+    const int row_len = 3;
+    
+    for (int i = 0; i < F.rows(); i++) 
+    {
+        // Sort the vector in ascending order to ensure
+        // there are no duplicate edges in the set
+        Eigen::Vector3i row = F.row(i);
+        sort(row.data(), row.data() + row.size());
+
+        edges.insert(tuple<int, int>(row[0], row[1]));
+        edges.insert(tuple<int, int>(row[0], row[2]));
+        edges.insert(tuple<int, int>(row[1], row[2]));
+	}
+    
+    Eigen::MatrixXi E(edges.size(), 2);
+    int cur_col = 0;
+
+    for (auto it = edges.begin(); it != edges.end(); it++) {
+        E(cur_col, 0) = get<0>(*it);
+        E(cur_col, 1) = get<1>(*it);
+        cur_col++;
+    }
+    
+    return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,23 @@
+#include "edges.h"
 #include "euler_characteristic.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi = 0;
-  // ADD YOUR CODE HERE
-  return Chi;
+    Eigen::MatrixXi E = edges(F);
+    int max_index = -1;
+
+    // Since edges() returns a matrix with sorted rows, 
+    // we know the highest index (which corresponds to
+    // the number of vertices) must be in the second column
+    for (int i = 0; i < E.rows(); i++) {
+        if (E(i, 1) > max_index) {
+            max_index = E(i, 1);
+        }
+    }
+
+    // Add 1 since indexing starts from 0
+    int V_size = max_index + 1;
+    
+    int Chi = V_size - E.rows() + F.rows();
+    return Chi;
 }
