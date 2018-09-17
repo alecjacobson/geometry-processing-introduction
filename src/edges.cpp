2498ac9c1833a1ebbde6d284952f3f79d72c7d54
Vineeth B <bhaskaravineeth@gmail.com>
2018-09-16 23:55:05 -0400

    Subrahmanya Vineeth Bhaskara HW00 Assignment Submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -3,6 +3,59 @@
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+
+  // The maximum bound on |E| is 3*|F|.
+  // Initialize E with an initial number of rows = 2*|F|.
+  E.Zero(F.rows()*2, 2);
+
+  // Variable maintaining the actual |E| as we iterate.
+  int num_edges = 0;
+
+  // Loop across the rows of F.
+  for (int row_index_f = 0; row_index_f < F.rows(); row_index_f++) {
+    
+    // this loop runs 3 times.
+    for (int col_index_f = 0; col_index_f < 3; col_index_f++) {
+      
+      // get the next index to form an edge (do arithmetic in mod 3 to form pairs of edges)
+      int next_col_index = (col_index_f + 1) % 3;
+
+      // Flag to check if the current edge is a duplicate (i.e., already exists in E)
+      // Initialize to false as we start with num_edges = 0.
+      bool isDuplicate = false;
+
+      // Check if E has to be resized
+      if (E.rows() == num_edges) {
+        // Do a conservative resize to increase rows by F.rows()
+        E.conservativeResize(E.rows() + F.rows(), 2);
+      } 
+
+      // Loop through E to check if duplicate.
+      // Check both the directions since E is unq list of undirected edges.
+      for (int row_index_e = 0; row_index_e < num_edges; row_index_e++) {
+        if ((E(row_index_e, 0) == F(row_index_f, col_index_f)) && (E(row_index_e, 1) == F(row_index_f, next_col_index))) {
+          isDuplicate = true;
+          break;
+        } else if ((E(row_index_e, 1) == F(row_index_f, col_index_f)) && (E(row_index_e, 0) == F(row_index_f, next_col_index))) {
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if (!isDuplicate) {
+        // If not a duplicate entry, add to the list of edges and update the counter num_edges.
+        E(num_edges, 0) = F(row_index_f, col_index_f);
+        E(num_edges, 1) = F(row_index_f, next_col_index);
+
+        num_edges++;
+      }
+      
+    } 
+
+  }
+
+  // If the final size is higher than actual num_edges, crop off garbage.
+  E.conservativeResize(num_edges, 2);
+
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,52 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
-  // ADD YOUR CODE HERE
+
+  // number of faces
+  int num_F = F.rows(); 
+
+  // Matrix with undirected edges.
+  Eigen::MatrixXi E = edges(F);
+
+  // number of edges
+  int num_E = E.rows(); 
+
+  // Construct a VectorXi that keeps track of unique vertex indices.
+  // Upper bound on the num of vertices = |E| * 2
+  Eigen::VectorXi V(num_E * 2);
+
+  // num_V tracks the number of unq vertices as we iterate over E
+  int num_V = 0;
+
+  for (int row_index_e = 0; row_index_e < num_E; row_index_e++) {
+    
+    // this loop runs twice. 
+    for (int col_index_e = 0; col_index_e < 2; col_index_e++) {
+
+      // check if the vertex is already present in the vertex list
+      bool isDuplicate = false;
+
+      for (int row_index_v = 0; row_index_v < num_V; row_index_v++) {
+        if (E(row_index_e, col_index_e) == V(row_index_v)) {
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if (!isDuplicate) {
+        V(num_V) = E(row_index_e, col_index_e);
+        num_V++;
+      }
+
+    }
+
+  }
+
+  // Compute Chi
+  Chi = num_V - num_E + num_F;
+
   return Chi;
 }
