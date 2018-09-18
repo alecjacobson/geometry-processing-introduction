51b5e62890a5d7fbf90ef01ae0b3185eb4162ff3
Yihuan Mao <yihuan.mao@mail.utoronto.ca>
2018-09-18 16:26:44 -0700

    submission

--- src/edges.cpp
+++ src/edges.cpp
@@ -2,7 +2,26 @@
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+  Eigen::MatrixXi E(F.rows()*3,2),E1(F.rows()*3/2,2);
+  int es=0;
+  bool flag;
+  for (int i=0; i<F.rows(); i++){
+    E(i*3,0)=F(i,0);
+    E(i*3,1)=F(i,1);
+    E(i*3+1,0)=F(i,1);
+    E(i*3+1,1)=F(i,2);
+    E(i*3+2,0)=F(i,2);
+    E(i*3+2,1)=F(i,0);
+  }
+  for (int i=0; i<F.rows(); i++){
+    flag=true;
+    for (int j=0; j<es; j++)
+      if (((E(i,0)==E1(j,0))&&(E(i,1)==E1(j,1)))||((E(i,0)==E1(j,1))&&(E(i,1)==E1(j,0)))) flag=false;
+    if (flag){
+      es++;
+      E1(es-1,0)=E(i,0);
+      E1(es-1,1)=E(i,1);
+    }
+  }
+  return E1;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -3,6 +3,20 @@
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
-  // ADD YOUR CODE HERE
+  int ee=F.rows()*3/2,ff=F.rows();
+  Eigen::MatrixXi dd(F.rows()*3,1);
+  int ds=0;
+  bool flag;
+  for (int i=0; i<F.rows(); i++)
+    for (int j=0; j<3; j++){
+      flag=true;
+      for (int k=0; k<ds; k++)
+        if (dd(ds,0)==F(i,j)) flag=false;
+      if (flag){
+        ds++;
+        dd(ds-1,0)=F(i,j);
+      }
+    }
+  Chi=ds-ee+ff;
   return Chi;
 }
