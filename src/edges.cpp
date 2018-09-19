169e918f8e6bae351308f497fdd39639b5765a01
Randy Hickey <randyhickey@Randy.local>
2018-09-18 22:44:44 -0400

    Finished

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,33 @@
 #include "edges.h"
+#include <algorithm>
+#include <set>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
-  return E;
+    Eigen::MatrixXi E;
+    std::set< std::pair<int,int> > fedges;
+    for (int i = 0; i < F.rows(); i++)
+    {
+        int a, b;
+        a = std::min(F(i, 0), F(i, 1));
+        b = std::max(F(i, 0), F(i, 1));
+        fedges.insert(std::make_pair(a,b));
+        a = std::min(F(i, 1), F(i, 2));
+        b = std::max(F(i, 1), F(i, 2));
+        fedges.insert(std::make_pair(a,b));
+        a = std::min(F(i, 2), F(i, 0));
+        b = std::max(F(i, 2), F(i, 0));
+        fedges.insert(std::make_pair(a,b));
+    }
+    Eigen::MatrixXi E2(fedges.size(), 2);
+    E = E2;
+    std::set< std::pair<int,int> >::iterator e;
+    int i = 0;
+    for (e = fedges.begin(); e != fedges.end(); ++e) {
+        std::pair<int,int> edge = *e;
+        E(i, 0) = edge.first;
+        E(i, 1) = edge.second;
+        i++;
+    }
+    return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,9 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
-  // ADD YOUR CODE HERE
+  Chi = edges(F).maxCoeff() + 1 - edges(F).rows() + F.rows();
   return Chi;
 }
