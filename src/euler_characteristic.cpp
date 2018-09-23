925ad1dfc2095bbb637dfcc9b8485bf92f3453e2
Amlan Kar <amlan.kar95@gmail.com>
2018-09-23 13:32:46 -0400

    Add euler_characteristic

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,16 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+
+  Eigen::MatrixXi E = edges(F);
+  int mod_V = 1 + F.maxCoeff();
+  // Assuming all indices are referenced
+
+  Chi = mod_V - E.rows() + F.rows();
+
   return Chi;
 }
