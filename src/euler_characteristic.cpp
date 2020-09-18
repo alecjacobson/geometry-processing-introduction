22ab9457a34fdafcf8dc9d64e6b595d8c8e00d9e
ahurka <hurka.alex@gmail.com>
2020-09-18 12:07:39 -0400

    Euler characteristic for unbroken meshes

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,18 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
-int euler_characteristic(const Eigen::MatrixXi &F)
-{
-  int Chi = 0;
-  // ADD YOUR CODE HERE
-  return Chi;
+int euler_characteristic(const Eigen::MatrixXi &F) {
+    int Chi = 0;
+
+    // ADD YOUR CODE HERE
+    int nFaces = F.rows();
+    // Search for max referenced vertex index.
+    int nVertices = F.maxCoeff() + 1;
+
+    Eigen::MatrixXi E = edges(F);
+    int nEdges = E.rows();
+
+    Chi = nVertices - nEdges + nFaces;
+
+    return Chi;
 }
